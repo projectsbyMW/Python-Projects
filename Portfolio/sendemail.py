7a38fd880fcf0b593a3cfe066eda9b5312c9f7c6
projectsbyMW <madheshwaran63@gmail.com>
2024-06-27 23:52:22 +0530

    Update sendemail.py

--- Portfolio/sendemail.py
+++ Portfolio/sendemail.py
@@ -4,10 +4,10 @@ def sendemail(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = SE_USERNAME
-    password = SE_PASSWORD
+    username = st.secrets[SE_USERNAME]
+    password = st.secrets[SE_PASSWORD]
 
-    receiver = "madheshwaran63@gmail.com"
+    receiver = st.secrets[SE_USERNAME]
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host,port, context = context) as server:
